b8b77933c2eb2987879ee62ca4430e98196a0e48
Bryan Soares <161507572+BryanSoares59d@users.noreply.github.com>
2025-02-28 18:22:04 -0300

    aula 03

---
+++ Aula_03/processa01.php
@@ -0,0 +1,4 @@
+<?php 
+   $nome = $_GET['cxnome'];
+   echo "Bem vindo $nome! \u{1F601}";
+?>
---
+++ Aula_03/processa_ex01.php
@@ -0,0 +1,5 @@
+<?php 
+   $nome = $_POST['cxnome'];
+   $msg = $_POST['cxmsg'];
+   echo "Olá $nome \u{1F601}!, foi um prazer receber sua seguinte mensagem: $msg";
+?>
